2e70e12a6a3e18be4a6b8124865443bb53608dca
Marco Walther <marco@sonic.net>
2024-02-26 22:24:55 -0800

    Do not add the ROS environment to root, I ran into trouble with /opt/ros/... shared libraries in the initrd. That did not work so well

--- image_builder/data/jammy-rt-humble/rootfs/etc/profile.d/99-source-ros.sh
+++ image_builder/data/jammy-rt-humble/rootfs/etc/profile.d/99-source-ros.sh
@@ -1,3 +1,7 @@
-if [ -f /opt/ros/humble/setup.bash ]; then
-  source /opt/ros/humble/setup.bash
+if [ $(id -u) -ne 0 ]
+then
+    if [ -f /opt/ros/humble/setup.bash ]
+    then
+        source /opt/ros/humble/setup.bash
+    fi
 fi
